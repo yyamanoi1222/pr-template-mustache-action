9520bd568cff6dcce27a6d2365a6a384d681e367
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-23 23:41:35 +0900

    dev

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import Mustache from 'mustache';
+
+const run = async () => {
+  const { context } = github;
+  const githubToken = core.getInput('token');
+  const client = github.getOctokit(githubToken).rest;
+
+  const { number: pull_number } = context.payload.pull_request;
+
+  const { data: pr } = await client.pulls.get({
+    ...context.repo,
+    pull_number,
+  });
+
+  const output = Mustache.render(pr.body || '', { ...process.env });
+
+  client.pulls.update({
+    ...context.repo,
+    pull_number,
+    body: output,
+  });
+};
+
+run();
