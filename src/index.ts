5ea94ef367076d4a9c5db0caf878816af68606ed
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-30 07:55:36 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ import * as Mustache from 'mustache';
 const run = async () => {
   const { context } = github;
   const githubToken = core.getInput('token');
-  const client = new github.getOctokit(githubToken);
+  const client = github.getOctokit(githubToken).rest;
 
   const { number: pull_number } = context.payload.pull_request;
 
-  const pr = client.pulls.get({
+  const pr = await client.pulls.get({
     ...context.repo,
     pull_number,
   });
 
-  const output = Mustache.render(pr.body, { ...process.env });
+  const output = Mustache.render(pr.data.body, { ...process.env });
 
   client.pulls.update({
     ...context.repo,
